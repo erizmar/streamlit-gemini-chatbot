528307b44bc77801676dc0fd141ff7ad9d49d473
erizmar <57591280+erizmar@users.noreply.github.com>
2024-12-13 06:57:48 +0000

    add session thread_id

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,6 @@
 from main import ChatBot
 import streamlit as st
+import random, string
 
 bot = ChatBot()
 
@@ -20,22 +21,35 @@ def generate_response(input, thread_id):
 if "messages" not in st.session_state.keys():
     st.session_state.messages = [{"role": "assistant", "content": "Welcome, I'm your assistant, ask me anything"}]
 
-# Display chat messages
-for message in st.session_state.messages:
-    with st.chat_message(message["role"]):
-        st.write(message["content"])
+if "thread_id" not in st.session_state.keys():
+    st.session_state.thread_id = ""
 
-# User-provided prompt
-if input := st.chat_input():
-    st.session_state.messages.append({"role": "user", "content": input})
-    with st.chat_message("user"):
-        st.write(input)
+# Click to generate new chat session
+trigger = st.button("New Chat")
 
-# Generate a new response if last message is not from assistant
-if st.session_state.messages[-1]["role"] != "assistant":
-    with st.chat_message("assistant"):
-        with st.spinner("Getting your answer from mystery stuff.."):
-            response = generate_response(input, "Ciso")
-            st.write(response)
-    message = {"role": "assistant", "content": response}
-    st.session_state.messages.append(message)
+if trigger:
+    st.session_state.thread_id = "CISO_".join(random.choices(string.ascii_uppercase + string.digits, k=7))
+    st.session_state.messages = [{"role": "assistant", "content": "Welcome, I'm your assistant, ask me anything"}]
+
+if st.session_state.thread_id != "":
+    st.write(f"Session ID: {st.session_state.thread_id}")
+
+    # Display chat messages
+    for message in st.session_state.messages:
+        with st.chat_message(message["role"]):
+            st.write(message["content"])
+
+    # User-provided prompt
+    if input := st.chat_input():
+        st.session_state.messages.append({"role": "user", "content": input})
+        with st.chat_message("user"):
+            st.write(input)
+
+    # Generate a new response if last message is not from assistant
+    if st.session_state.messages[-1]["role"] != "assistant":
+        with st.chat_message("assistant"):
+            with st.spinner("Getting your answer from mystery stuff.."):
+                response = generate_response(input, st.session_state.thread_id)
+                st.write(response)
+        message = {"role": "assistant", "content": response}
+        st.session_state.messages.append(message)
